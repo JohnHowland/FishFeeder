733b657b2b1e99cbaed3c6f07f5ed801772a0877
John Howland <john.howland02@gmail.com>
2021-10-24 00:20:10 -0400

    inital commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT)
+
+try:  
+    while True:  
+        GPIO.output(7, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
+        sleep(2.0)                 # wait half a second  
+        if GPIO.input(7):  
+            print("LED just about to switch off" )
+        GPIO.output(7, 0)         # set GPIO24 to 0/GPIO.LOW/False  
+        sleep(2.0)                 # wait half a second  
+        if not GPIO.input(7):  
+            print("LED just about to switch on" ) 
+
+except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
+    GPIO.cleanup()                 # resets all GPIO ports used by this program
