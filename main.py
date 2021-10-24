c3550b4cfabee9ad5075cfd90ef2eca4235d028a
John Howland <john.howland02@gmail.com>
2021-10-24 00:22:51 -0400

    forgot time

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ GPIO.setup(7, GPIO.OUT)
 try:  
     while True:  
         GPIO.output(7, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
-        sleep(2.0)                 # wait half a second  
+        time.sleep(2.0)                 # wait half a second  
         if GPIO.input(7):  
             print("LED just about to switch off" )
         GPIO.output(7, 0)         # set GPIO24 to 0/GPIO.LOW/False  
-        sleep(2.0)                 # wait half a second  
+        time.sleep(2.0)                 # wait half a second  
         if not GPIO.input(7):  
             print("LED just about to switch on" ) 
 
