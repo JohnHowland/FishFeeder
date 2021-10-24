7f2463f438f3123a335afa6b695da630a294e13e
John Howland <john.howland02@gmail.com>
2021-10-24 00:27:52 -0400

    BOARD to BCM

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.OUT)
 
 try:  
     while True:  
-        GPIO.output(7, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
+        GPIO.output(4, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
         time.sleep(2.0)                 # wait half a second  
-        if GPIO.input(7):  
+        if GPIO.input(4):  
             print("LED just about to switch off" )
-        GPIO.output(7, 0)         # set GPIO24 to 0/GPIO.LOW/False  
+        GPIO.output(4, 0)         # set GPIO24 to 0/GPIO.LOW/False  
         time.sleep(2.0)                 # wait half a second  
-        if not GPIO.input(7):  
+        if not GPIO.input(4):  
             print("LED just about to switch on" ) 
 
 except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
